95ec3d3e56d159de3e63838d312760e0d2d33ee9
Zenxdkd <99894254+Zenxdkd@users.noreply.github.com>
2023-04-02 13:28:00 +0200

    Delete jajaj.lua

--- jajaj.lua
+++
@@ -1,9 +0,0 @@
-local HWIDTable = loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenxdkd/test/main/whitelist.lua"))()
-local HWID = game:GetService("RbxAnalyticsService"):GetClientId()
-for i,v in pairs(HWIDTable) do
-    if v == HWID then
-        print("Loading Script...")
-        wait(3)
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenxdkd/test/main/klxofullstremeable.lua"))()
-    end  
-end
