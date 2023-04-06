2fe5c36ddc9613e4ae43beb250e4b97c9d6f38c0
Zenxdkd <99894254+Zenxdkd@users.noreply.github.com>
2023-04-06 15:54:34 +0200

    Update klox.lua

--- klox.lua
+++ klox.lua
@@ -60,7 +60,7 @@ end
 
 uwuMisc:Toggle({
 Name = "Shake",
-Default = true,
+Default = false,
 Pointer = "Shake_Enabled",
 Callback = function(v)
 print(v)
@@ -83,7 +83,7 @@ end
 uwuSettings:Slider({Name = "Smoothness",
 Minimum = 0.001,
 Maximum = 1.5,
-Default = 0.025,
+Default = 0.0535,
 Decimals = 0.001,
 Pointer = "AimbotMain_Smoothness",
 Callback = function(v)
@@ -95,7 +95,7 @@ end
 uwuSettings:Slider({Name = "FOV Radius",
 Minimum = 0.01,
 Maximum = 100,
-Default = 25,
+Default = 50,
 Decimals = 0.01,
 Pointer = "AimbotMain_Radius",
 Callback = function(v)
@@ -119,7 +119,7 @@ end
 uwuSettings:Slider({Name = "Tracer Prediction",
 Minimum = 0.001,
 Maximum = 10,
-Default = 0.19,
+Default = 0.065,
 Decimals = 0.001,
 Pointer = "AimbotMain_Booty",
 Callback = function(v)
@@ -131,7 +131,7 @@ end
 uwuSettings:Slider({Name = "Silent Prediction",
 Minimum = 0.10,
 Maximum = 0.195,
-Default = 0.128,
+Default = 0.135,
 Decimals = 0.001,
 Pointer = "AimbotMain_Booty",
 Callback = function(v)
